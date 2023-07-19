f14466d628ef5175621e86a6f339de05f7602ec9
rlaoxdn <rlaxodn0215@naver.com>
2023-07-19 11:06:51 +0900

    server

---
+++ 4. WindowAPI/Net_Client/Net_Client.cpp
@@ -0,0 +1,95 @@
+﻿// Net_Client.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include<stdlib.h>
+#include<string.h>
+
+#include<WinSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+#define Buf_Size 1024
+
+char strServerIP[] = "172.30.1.54";
+char strPort[] = "12345";
+
+void Error(const char* msg);
+void Log(const char* msg);
+void Client_Code();
+
+int main()
+{
+    std::cout << "Client Start!!\n";
+    Client_Code();
+    return 0;
+}
+
+void Client_Code()
+{
+
+    WSADATA wsaData;
+    SOCKET serverSocket;
+    char msg[Buf_Size];
+    int strLength;
+    SOCKADDR_IN serverAddr;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        Error("WSAStartup() Error");
+    }
+
+    Log("Start....");
+
+    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        Error("Socket() Error");
+    }
+
+    Log("socket() OK");
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.S_un.S_addr = inet_addr(strServerIP);
+    serverAddr.sin_port = htons(atoi(strPort));
+
+    if (connect(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        ERROR("Conneted() Error");
+    }
+
+    Log("Connected....");
+
+    while (1)
+    {
+        fputs("Input Message(Q to quit) : ", stdout);
+        fgets(msg, Buf_Size, stdin);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) break;
+
+        send(serverSocket, msg, strlen(msg), 0);
+        strLength = recv(serverSocket, msg, Buf_Size - 1, 0);
+        msg[strLength] = 0;
+        printf("Message from Server : %s\n", msg);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+}
+
+void Error(const char* msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void Log(const char* msg)
+{
+    fputs(msg, stdout);
+    fputc('\n', stdout);
+}
+
+
+
---
+++ 4. WindowAPI/Net_Server/Net_Server.cpp
@@ -0,0 +1,115 @@
+﻿// Net_Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include<stdlib.h>
+#include<string.h>
+
+#include<WinSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+#define Buf_Size 1024
+void Error(const char* msg);
+void Log(const char* msg);
+
+char strServerIP[] = "172.30.1.54";
+char strPort[] = "12345";
+
+void Server_Code();
+
+int main()
+{
+    std::cout << "Server Start!\n";
+    Server_Code();
+    return 0;
+}
+
+
+void Server_Code()
+{
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    char msg[Buf_Size];
+    int strLength, i;
+
+    SOCKADDR_IN serverAddr, clientAddr;
+    int clientAddrSize;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ERROR("WSAStartup() Error");
+    }
+    Log("WSAStartup() OK");
+
+    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket == INVALID_SOCKET)
+    {
+        Error("socket() error");
+    }
+
+    Log("socket() OK");
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(atoi(strPort));
+
+    if (bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        ERROR("bind() Error");
+    }
+
+    Log("bind() OK");
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        Error("listen() Error");
+    }
+
+    Log("listen() OK");
+
+    clientAddrSize = sizeof(clientAddr);
+    for (int i = 0; i < SOMAXCONN; i++)
+    {
+        clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddr, &clientAddrSize);
+        Log("accept()...");
+
+        if (clientSocket == SOCKET_ERROR)
+            Error("accept() Error");
+        else
+            printf("Connencted client %d\n", i + 1);
+
+        memset(msg, 0, sizeof(msg));
+
+        while ((strLength = recv(clientSocket, msg, Buf_Size, 0)) != 0)
+        {
+            printf("%d client : %s", i + 1, msg);
+            send(clientSocket, msg, strLength, 0);
+            memset(msg, 0, sizeof(msg));
+
+        }
+
+        closesocket(clientSocket);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    Log("Finish...");
+
+}
+
+void Error(const char* msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void Log(const char* msg)
+{
+    fputs(msg, stdout);
+    fputc('\n', stdout);
+}
