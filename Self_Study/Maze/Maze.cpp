600b142ab7fb723b6513e55bbfd9be1b967abced
rlaoxdn <rlaxodn0215@naver.com>
2023-09-02 17:10:24 +0900

    123

---
+++ Self_Study/Maze/Maze.cpp
@@ -0,0 +1,30 @@
+﻿#include"pch.h"
+#include <iostream>
+#include"ConsoleHelper.h"
+#include"Board.h"
+
+Board board;
+
+int main()
+{
+    board.Init(25);
+
+    uint64 lastTick = 0;
+    while (true)
+    {
+#pragma region 프레임 관리
+        const uint64 currentTick = ::GetTickCount64();
+        const uint64 deltaTick = currentTick - lastTick;
+        lastTick = currentTick;
+#pragma endregion
+
+        // 입력
+
+        // 로직
+
+        // 랜더링
+        board.Render();
+    }
+
+}
+
