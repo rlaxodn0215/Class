192f19d59db6a05e793679abec290b9fa0b8edb1
rlaoxdn <rlaxodn0215@naver.com>
2023-09-05 20:53:39 +0900

    132

--- Self_Study/Maze/Player.h
+++ Self_Study/Maze/Player.h
@@ -4,15 +4,27 @@ class Board;
 
 class Player
 {
+	enum
+	{
+		MOVE_TICK = 100
+	};
+
 public:
 	void Init(Board* board);
 	void Update(uint64 deltaTick);
 
 	void SetPos(Pos pos) { _pos = pos; }
 	Pos GetPos() { return _pos; }
+
+	bool CanGo(Pos pos);
+
 private:
 	Pos _pos = {};
 	int32 _dir = DIR_UP;
 	Board* _board = nullptr;
+
+	vector<Pos> _path;
+	int32		_pathIndex = 0;
+	uint64		_sumTick = 0;
 };
 
