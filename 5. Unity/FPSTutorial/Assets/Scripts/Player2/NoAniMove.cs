4b836430fb1e28271b3630d30e58d2b8ba67ddc7
rlaoxdn <rlaxodn0215@naver.com>
2023-10-09 12:12:56 +0900

    asd

--- 5. Unity/FPSTutorial/Assets/Scripts/Player2/NoAniMove.cs
+++ 5. Unity/FPSTutorial/Assets/Scripts/Player2/NoAniMove.cs
@@ -4,6 +4,7 @@ public class NoAniMove : MonoBehaviour
 {
     void Update()
     {
-        transform.position = new Vector3(0, 0, 0);
+        transform.localPosition = new Vector3(0, 0, 0);
     }
 }
+
