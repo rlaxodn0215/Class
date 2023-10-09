c3bfd522b2b1a1fe2f53f75f1b43836f1912370c
rlaoxdn <rlaxodn0215@naver.com>
2023-10-09 20:07:02 +0900

    123

--- 5. Unity/FPS_Test/Assets/Scripts/PlayerSetup.cs
+++ 5. Unity/FPS_Test/Assets/Scripts/PlayerSetup.cs
@@ -6,11 +6,11 @@ public class PlayerSetup : MonoBehaviour
 {
     public PlayerMove playerMove;
     public GameObject camera;
-    //public GameObject UIs;
+    public GameObject UIs;
     public void IsLocalPlayer()
     {
         playerMove.enabled = true;
         camera.SetActive(true);
-        //UIs.SetActive(true);
+        UIs.SetActive(true);
     }
 }
