a5911da8da7d8de783ed8de610a9c324bc359cc0
rlaoxdn <rlaxodn0215@naver.com>
2023-10-09 20:39:20 +0900

    asd

--- 5. Unity/FPS_Test/Assets/Scripts/DestroyPartical.cs
+++ 5. Unity/FPS_Test/Assets/Scripts/DestroyPartical.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+
 
 public class DestroyPartical : MonoBehaviour
 {
     IEnumerator Start()
     {
         yield return new WaitForSeconds(1f);
+        GetComponent<PhotonView>().RPC("Gone", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    public void Gone()
+    {
         Destroy(gameObject);
     }
 
